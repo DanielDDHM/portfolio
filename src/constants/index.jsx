1a0d46c2d51ad03ad504c4bba1fdc6abafa4902f
DanielDDHM <daniel.ddhm@gmail.com>
2022-08-17 11:27:16 -0300

    Initial commit
    Generated by create-expo-app 1.1.1.

--- src/constants/index.jsx
+++
@@ -1,3 +0,0 @@
-export * from './default';
-export * from './experience';
-export * from './education';
--- src/pages/index.jsx
+++
@@ -1,5 +0,0 @@
-export * from './about/';
-export * from './experience';
-export * from './education';
-export * from './skills';
-export * from './contact';
